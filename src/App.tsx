c4f45bf6715b44190950cac156cedc255a5f91b4
matouka-suzuki <real.matouka.suzuki@gmail.com>
2017-12-01 00:13:18 +0900

    Props,Stateのinterfaceを定義　

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,14 @@ import {
   View
 } from 'react-native';
 
+interface Props {
+
+}
+
+interface State {
+
+}
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -19,7 +27,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends React.Component<object, object> {
+export default class App extends React.Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
